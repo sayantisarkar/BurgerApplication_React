ed69ed17a4224dbabaedd78cc763c9279183c82b
sayantisarkar <sayantisarkar088@gmail.com>
2021-06-19 18:21:03 +0400

    Fixed typo

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -26,7 +26,7 @@ const buildControls = props => (
             ))}
             <button className={classes.OrderButton} 
             disabled={!props.purchasable} 
-            onClick={props.onPuchase}>ORDER NOW</button>
+            onClick={props.onPurchase}>ORDER NOW</button>
         </div>
 );
 
